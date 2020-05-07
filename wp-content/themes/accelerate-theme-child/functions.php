7848b474b9bfbf2d495d2e3436ae0b61076bacd8
jmccormack <jessmccormack.design@gmail.com>
2020-05-07 12:06:02 -0400

    Contact us page

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -37,5 +37,15 @@ function create_custom_post_types() {
 				);
 }
 
+add_filter( 'body_class','accelerate_child_body_classes' );
+function accelerate_child_body_classes( $classes ) {
+ if (is_page('contact-us') ) {
+   $classes[] = 'contact-us';
+ }
+
+   return $classes;
+
+}
+
 // Hook this custom post type function into the theme
 add_action( 'init', 'create_custom_post_types' );
